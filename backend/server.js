c5e22890d897158a7f4eea0429c3e6a8c49bed51
dhruv33333 <dhruvnaveen.440@gmail.com>
2022-04-04 17:38:20 +0530

    added initial ui and api's

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/mongoose");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json()); //to accept json data
+
+app.get("/", (req, res) => {
+  res.send("<h1> ur</h1>");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
---
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -0,0 +1,94 @@
+import { useDisclosure } from "@chakra-ui/hooks";
+import { ViewIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useToast,
+} from "@chakra-ui/react";
+import { ChatState } from "../../Context/ChatProvider";
+import UserBadgeItem from "../UserAvatar/UserBadgeItem";
+import React, { useState } from "react";
+
+const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState();
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [renameLoading, setRenameLoading] = useState(false);
+
+  const toast = useToast();
+
+  const { selectedChat, setSelectedChat, user } = ChatState();
+
+  const handleRemove = () => {};
+  const handleRename = () => {};
+
+  return (
+    <>
+      <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="Work sans"
+            d="flex"
+            justifyContent="center"
+          >
+            {selectedChat.chatName}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box w="100%" d="flex" flexWrap="wrap" pb={3}>
+              {selectedChat.users.map((user) => (
+                <UserBadgeItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => handleRemove(user)}
+                />
+              ))}
+            </Box>
+
+            <FormControl d="flex">
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                value={groupChatName}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+              <Button
+                variant="solid"
+                colorScheme="teal"
+                ml={1}
+                isLoading={renameLoading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default UpdateGroupChatModal;
